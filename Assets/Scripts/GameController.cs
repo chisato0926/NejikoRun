0d2e2dae050f58170a0f6e32192147d52d997c39
chisato <chisatokobo@gmail.com>
2022-02-09 13:52:54 +0900

    p311まで作成

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        //スコアを更新
+        int score = CalcScore();
+        scoreText.text = $"Score:{score}m";
+        //ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore() {
+        //ねじ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
