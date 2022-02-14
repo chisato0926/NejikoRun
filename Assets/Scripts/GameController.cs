a964e342f6af228a6452cc42ba424e4ceb3dd8a1
chisato <chisatokobo@gmail.com>
2022-02-14 11:01:32 +0900

    完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -19,12 +20,30 @@ public class GameController : MonoBehaviour
         //スコアを更新
         int score = CalcScore();
         scoreText.text = $"Score:{score}m";
+
         //ライフパネルを更新
         lifePanel.UpdateLife(nejiko.Life());
+
+        //ねじ子のライフが0になったらゲームオーバー
+        if (nejiko.Life() <= 0) {
+            //これ以降のUpdateは止める
+            enabled = false;
+            //ハイスコアを更新
+            if (PlayerPrefs.GetInt("HighScore") < score) {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            //2秒後にReturnToTitleを呼び出す
+            Invoke("ReturnToTitle", 2.0f);
+        }
     }
 
     int CalcScore() {
         //ねじ子の走行距離をスコアとする
         return (int)nejiko.transform.position.z;
+    }
+
+    void ReturnToTitle() {
+        //タイトルシーンに切り替え
+        SceneManager.LoadScene("Title");
     }
 }
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    public Text highScoreText;
+
+    public void Start() {
+        //ハイスコアを表示
+        highScoreText.text = "Heigh Score:" + PlayerPrefs.GetInt("HighScore") + "m";
+    }
+
+    public void OnStartButtonClicked() {
+        SceneManager.LoadScene("Main");
+    }
+}
